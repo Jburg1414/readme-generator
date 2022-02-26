0e6cc4855f878ed490fe6e3d7d1baed99896cc86
Jburg1414 <Jburgess1414@gmail.com>
2022-02-26 14:01:21 -0700

    finished adding questions for readme

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = () => {
   return inquire.prompt([
     {
+        type: "input",
+        name: "githubUsername",
+        message: "Please enter your GitHub username. (Required)",
+        validate: (githubUsername) => {
+            if (githubUsername) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address for additional questions. (Required)",
+        validate: (email) => {
+            if (email) {
+                return true;
+            } else {
+                console.log("Please enter an email address!")
+                return false;
+            }
+        }
+    },
+    {
       type: "input",
       name: "project",
       message: "What is the title of your project? (Required)",
@@ -35,15 +61,70 @@ const questions = () => {
     {
         type: "confirm",
         name: "installation",
-        message: "Does your project require installation instructions?",
+        message: "Does your project require installation instructions? (Required)",
         default: true,
     },
     {
         type: "input",
         name: "installationInstructions",
         message: "What are the installation instructions for your project?",
-        // when: 
+        when: ({installation}) => {
+            if (installation) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the reason for this project? Enter how and why to use this project. (Required)",
+      validate: (usage) => {
+          if (usage) {
+              return true;
+          } else {
+              console.log("Please enter a reason for this project?")
+              return false;
+          }
+      }  
+    },
+    {
+        type: "input",
+        name: "contributor",
+        message: "Please enter all the contributors for this project. (Required)",
+        validate: (contributor) => {
+            if (contributor) {
+                return true;
+            } else {
+                console.log("Please enter all the contributors!")
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please enter the test instructions for this project. (Required)",
+        validate: (test) => {
+            if (test) {
+                return true;
+            } else {
+                console.log("Please enter the test instructions!")
+                return false;
+            }
+        }
+    },
+    {
+        type: "checkbox",
+        name: "projectLicense",
+        message: "Select the 'Licenses' that need to be included with this project. (Select all that apply)",
+        choices: [
+            "MIT",
+            "APACHE",
+            "GNU"
+        ],
+    }
   ]);
 };
 // TODO: Create a function to write README file
